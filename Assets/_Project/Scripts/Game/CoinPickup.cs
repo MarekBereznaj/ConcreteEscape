8a75963c75e2d8533eca49816dcaef214c8c3811
MarekBereznaj <marek.bereznaj@gmail.com>
2026-01-18 18:30:02 +0100

    Add coin

---
+++ Assets/_Project/Scripts/Game/CoinPickup.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public float rotateSpeed = 120f;
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        if (CoinManager.Instance != null)
+            CoinManager.Instance.CollectOne();
+
+        Destroy(gameObject);
+    }
+}
